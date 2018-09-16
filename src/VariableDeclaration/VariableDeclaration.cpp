4075a74a8399185eaac3559c198e2fcb0109dbb2
Vasile <vasilerotaru38@gmail.com>
2018-09-16 15:43:23 +0300

    Major changes

--- src/VariableDeclaration/VariableDeclaration.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by vasile on 16/09/18.
-//
-
-#include "VariableDeclaration.h"
--- src/main_source.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by vasile on 16/09/18.
-//
-
-int main(int argc, char **argv) {
-    std::cout << "Hello, source mate!" << std::endl;
-    return 0;
-}
